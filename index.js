a0826ae6130eae5e78c06b6e50d34e898f835b40
donte hightower <dontedhightower@gmail.com>
2018-02-11 22:39:42 -0600

    handle tie scenario properly

--- index.js
+++ index.js
@@ -10,40 +10,51 @@ const rockPaperScissors = {
   },
 
   computerPlay: function(){
-    let generator = (Math.random() * 10).toFixed;
-    if (generator < 3) {
+    let generator = (Math.random() * 10).toFixed();
+    if (generator <= 3) {
       return 'rock';
-    } else if (generator > 7){
+    } else if (generator <= 6){
       return 'scissors';
-    } else {
+    } else if (generator <= 10) {
       return 'paper';
     }
   },
 
-    playRound: function(playerSelection, computerSelection){
-    if (playerSelection == "paper" && computerSelection == "rock") {
-      playerWins = true;
-      return "You Win! Paper beats Rock";
-    } else if (playerSelection === "rock" && computerSelection === "paper"){
-      playerWins = false;
-      return "You Lose! Paper beats Rock";
-    } else if (playerSelection === "scissors" && computerSelection === "paper"){
-      playerWins = true;
-      return "You Win! Scissors beats Paper!";
-    } else if (playerSelection === "paper" && computerSelection === "scissors"){
-      playerWins = false;
-      return "You Lose! Scissors beats Paper!";
-    } else if (playerSelection === "rock" && computerSelection === "scissors"){
-      playerWins = true;
-      return "You Win! Rock beats Scissors!";
-    } else if (playerSelection === "scissors" && computerSelection ==="rock"){
-      playerWins = false; 
-      return "You Lose! Rock Beats Scissors!"
+  playRound: function(playerSelection, computerSelection){
+    if(playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors"){
+      if (playerSelection === "paper" && computerSelection === "rock") {
+        this.playerWins = true;
+        console.log( "You Win! Paper beats Rock");
+      } else if (playerSelection === "rock" && computerSelection === "paper"){
+        this.playerWins = false;
+        console.log( "You Lose! Paper beats Rock");
+      } else if (playerSelection === "scissors" && computerSelection === "paper"){
+        this.playerWins = true;
+        console.log( "You Win! Scissors beats Paper!");
+      } else if (playerSelection === "paper" && computerSelection === "scissors"){
+        this.playerWins = false;
+        console.log( "You Lose! Scissors beats Paper!");
+      } else if (playerSelection === "rock" && computerSelection === "scissors"){
+        this.playerWins = true;
+        console.log( "You Win! Rock beats Scissors!");
+      } else if (playerSelection === "scissors" && computerSelection ==="rock"){
+        this.playerWins = false; 
+        console.log("You Lose! Rock Beats Scissors!");
+      } else {
+        this.playerWins = undefined;
+        console.log("It's a tie!")
+      }
+    } else {
+      playerSelection = prompt("Invalid Choice, please select rock, paper, or scissors");
     }
   },
 
   keepScore: function() {
-    this.playerWins === true ? this.score.playerScore++ : this.score.computerScore++;
+    if(this.playerWins === true){
+      this.score.playerScore++;
+    } else if (this.playerWins === false){
+      this.score.computerScore++;
+    }
     console.log(this.score);
   },
 
@@ -53,7 +64,6 @@ const rockPaperScissors = {
       this.playRound(this.getPlayerSelection().toLowerCase(), this.computerPlay())
       this.keepScore();
     }
-    //report winner or loser at the end
   }
 }
 rockPaperScissors.fiveRoundGame();
